e5e92d041df3ebac6d7a3ea79cc0ec94a8d1bf13
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-03-04 16:51:04 -0500

    Scratch code looking at CESM

--- explore-cesm.R
+++ explore-cesm.R
@@ -1,1 +1,6 @@
-renv::install("ashiklom/pecanrtm")
+library(ncdf4)
+
+bfile <- "data/cesm/b.e11.B1850C5CN.f09_g16.005.clm2.h0.LAISHA.200001-209912.nc"
+ffile <- "data/cesm/f.e11.F1850C5CN.f09_f09.001.clm2.h0.LAISHA.200001-209912.nc"
+
+bnc <- nc_open(bfile)
