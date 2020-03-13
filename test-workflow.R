9ad0917f3d8a985455cc95b8a918c3351db4b4b8
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-03-13 09:30:22 -0400

    Randomize PROSPECT parameters

--- test-workflow.R
+++ test-workflow.R
@@ -23,12 +23,39 @@ phi <- 0.05
 # Use lognormal distribution here
 # Mean LAI is exp(0.5) ~~ 1.65
 X <- exp(rmvn(1, rep(0.5, n), 0.2 * exp(-phi * distance)))
-## hist(X, xlab = "LAI")
 
-Xraster <- rasterFromXYZ(cbind(simgrid - 0.5, t(X)))
-## plot(Xraster)
+lai <- rasterFromXYZ(cbind(simgrid - 0.5, t(X)))
 
-lai <- Xraster
+# Random truncated normal
+rtnorm <- function(n, mu, sigma) {
+  x <- rnorm(n, mu, sigma)
+  while (any(x < 0)) {
+    x[x < 0] <- rnorm(sum(x < 0), mu, sigma)
+  }
+  x
+}
+
+# PROSPECT parameter distributions
+drawN <- function(n = 1) 1 + rlnorm(n, 0.3, 0.2)
+drawCab <- function(n = 1) rtnorm(n, 40, 10)
+drawCar <- function(n = 1) rlnorm(n, 1, 1)
+drawCbrown <- function(n = 1) rbeta(n, 0.9, 1)
+drawCw <- function(n = 1) rlnorm(n, log(0.01), 1)
+drawCm <- function(n = 1) rlnorm(n, log(0.01), 1)
+
+# Draw random values for PROSPECT parameters
+ni <- NROW(lai)
+nj <- NCOL(lai)
+nn <- ni * nj
+N <- matrix(drawN(nn), ni, nj)
+Cab <- matrix(drawCab(nn), ni, nj)
+Car <- matrix(drawCar(nn), ni, nj)
+Cbrown <- matrix(drawCbrown(nn), ni, nj)
+Cw <- matrix(drawCw(nn), ni, nj)
+Cm <- matrix(drawCm(nn), ni, nj)
+
+lidfa <- matrix(runif(nn, -0.4, -0.3), ni, nj)
+lidfb <- matrix(runif(nn, -0.2, -0.1), ni, nj)
 
 # Now, try using this LAI field to run PRO4SAIL
 wl <- 400:2500
@@ -37,14 +64,16 @@ wl <- 400:2500
 # 2 - Hemispherical-directional
 # 3 - Directional-hemispherical
 # 4 - Bi-directional
-result <- array(numeric(), c(NROW(lai), NCOL(lai), length(wl), 4))
+result <- array(numeric(), c(ni, nj, length(wl), 4))
 pb <- progress::progress_bar$new(total = length(lai))
-for (i in 1:NROW(lai)) {
-  for (j in 1:NCOL(lai)) {
+for (i in seq_len(ni)) {
+  for (j in seq_len(nj)) {
     result[i, j, ,] <- PEcAnRTM::pro4sail(c(
-      N = 1.4, Cab = 40, Car = 8, Cbrown = 0,
-      Cw = 0.01, Cm = 0.01,
-      LIDFa = -0.35, LIDFb = -0.15, TypeLIDF = 1,
+      ## N = 1.4, Cab = 40, Car = 8, Cbrown = 0,
+      ## Cw = 0.01, Cm = 0.01,
+      N = N[i, j], Cab = Cab[i, j], Car = Car[i, j], Cbrown = Cbrown[i, j],
+      Cw = Cw[i, j], Cm = Cm[i, j],
+      LIDFa = lidfa[i, j], LIDFb = lidfb[i, j], TypeLIDF = 1,
       LAI = lai[i,j],
       q = 0.01, tts = 30, tto = 10,
       psi = 0, psoil = 0.7
@@ -80,12 +109,21 @@ hdr_bdr <- c(
 writeLines(hdr_bdr, file.path(outdir, "prosail-bdr.bsq.hdr"))
 writeLines(hdr_bdr, file.path(outdir, "prosail-hdr.bsq.hdr"))
 
-## result_nir <- result[,, 380, 4]
-## result_r <- result[,, 270, 4]
-## result_ndvi <- (result_nir - result_r) / (result_nir + result_r)
-## result_ndvi_raster <- raster(result_ndvi, xmn = 1, xmx = 50, ymn = 1, ymx = 50)
-## par(mfrow = c(1, 3))
-## plot(result_ndvi_raster, main = "NDVI")
-## plot(Xraster, main = "True LAI")
-## plot(getValues(result_ndvi_raster), getValues(Xraster),
-##      xlab = "NDVI", ylab = "LAI")
+# ...and save the true values for validation, etc.
+true_values <- list(
+  lai = as.matrix(lai), N = N, Cab = Cab, Car = Car, Cbrown = Cbrown,
+  Cw = Cw, Cm = Cm, lidfa = lidfa, lidfb = lidfb
+)
+saveRDS(true_values, file.path(outdir, "true-values.rds"))
+
+if (interactive()) {
+  result_nir <- result[,, 380, 4]
+  result_r <- result[,, 270, 4]
+  result_ndvi <- (result_nir - result_r) / (result_nir + result_r)
+  result_ndvi_raster <- raster(result_ndvi, xmn = 1, xmx = 50, ymn = 1, ymx = 50)
+  par(mfrow = c(1, 3))
+  plot(lai, main = "True LAI")
+  plot(result_ndvi_raster, main = "NDVI")
+  plot(getValues(result_ndvi_raster), getValues(lai),
+       xlab = "NDVI", ylab = "LAI")
+}
